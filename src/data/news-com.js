997ad0a67bfa283e96984f4ddac99f02cc7df7a7
Aya Barni <aya.barni98@gmail.com>
2026-06-09 10:58:14 +0200

    update news page compact versie

---
+++ src/data/news-com.js
@@ -0,0 +1,37 @@
+export const newsArticles = [
+  {
+    title: "Burst Your Bubble 040 Pitch Night",
+    intro:
+      "Bij de Burst Your Bubble 040 Pitch Night presenteerden 19 inwoners van Eindhoven hun ideeën om de stad inclusiever en meer verbonden te maken.",
+    body:
+        "Tijdens de Burst Your Bubble 040 Pitch Night werden 19 ideeën gepresenteerd door creatieve en verbindende inwoners met doel Eindhoven de meest inclusieve stad van Nederland maken De jury selecteerde de top 6, waaruit het publiek uiteindelijk drie winnaars koos. De ideeën richten zich op ontmoeting, bewustwording en inclusie"
+  },
+  {
+    title: "Blind Sherlock",
+    intro:
+      "Blind Sherlock toont zowel de kracht als de innerlijke worsteling van mensen met een visuele beperking.",
+    body:
+     "Blind Sherlock is een serie op Netflix die gaat over een man die visueel beperkt is en bij de politie solliciteert om zijn bijzondere natuurlijke gave te laten blijken.De serie maakt stereotypering zichtbaar en laat zien hoe mensen met een visuele beperking vaak anders worden bekeken. Het verhaal draagt bij aan meer bewustwording en inclusie. Het interessante aan het verhaal is dat het niet alleen gaat over wat iemand niet kan, maar juist over talent, perspectief en de manier waarop iemand kan bijdragen aan de samenleving" 
+ },
+  {
+    title: "Taal, Communicatie en Verbeeldingskracht",
+    intro:
+      "Sleutels tot zelfvertrouwen voor mensen met een visuele beperking.",
+    body:
+     "Taal en communicatie spelen een grote rol in hoe mensen zichzelf en anderen begrijpen. Voor mensen met een visuele beperking kan duidelijke communicatie bijdragen aan zelfvertrouwen, zelfstandigheid en verbinding. Door verhalen, verbeeldingskracht en toegankelijke informatie kunnen mensen zich beter herkennen, uitdrukken en deelnemen aan de samenleving."
+  }, 
+  {
+    title: "Nieuw bestuurslid Roger Ravelli stelt zich voor",
+    intro:
+      "In dit nieuwsartikel maak je kennis met Roger Ravelli. Hij stelt zich voor als nieuw bestuurslid en geeft een inkijkje in zijn profiel en achtergrond.",
+    body:
+     "Roger Ravelli groeide op in Bilthoven, als praktische denker en doener in een gezin vol academici. Na de MTS en later de HTS Werktuigbouwkunde werkte hij in verschillende technische en creatieve omgevingen. Zijn persoonlijke achtergrond leert hem dat beperkingen vaak deuren openen naar nieuwe perspectieven. Met zijn ervaring wil hij bijdragen aan inclusie, samenwerking en vernieuwing binnen Videlio."
+ },
+  {
+    title: "De werkelijke betekenis van 'handicap'",
+    intro:
+      "Beeldvorming is geen bijzaak - het is de werkelijkheid.",
+    body:
+      "De manier waarop we spreken over handicap heeft invloed op hoe mensen worden gezien en behandeld. Het gaat niet alleen om fysieke of visuele beperkingen, maar ook om de omgeving die mensen soms beperkt. Door bewuster om te gaan met taal, beeldvorming en toegankelijkheid ontstaat er meer ruimte voor gelijkwaardige deelname."
+},
+];
